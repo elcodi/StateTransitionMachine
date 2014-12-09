solilokiam/elcodi

State Transitional Machine for Elcodi.
This is an implementation of a State Transitional Machine and the intergration
with given Elcodi implementation.

src/Elcodi/Component/StateTransitionMachine/Event/InitializationEvent.php
@@ -1,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\StateTransitionMachine\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+use Elcodi\Component\StateTransitionMachine\Entity\Interfaces\StateableInterface;
+
+/**
+ * Class InitializationEvent
+ */
+class InitializationEvent extends Event
+{
+    /**
+     * @var StateableInterface
+     *
+     * Object
+     */
+    protected $object;
+
+    /**
+     * Construct
+     *
+     * @param StateableInterface $object Object
+     */
+    public function __construct(StateableInterface $object)
+    {
+        $this->object = $object;
+    }
+
+    /**
+     * Get Object
+     *
+     * @return StateableInterface Object
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+}

src/Elcodi/Component/StateTransitionMachine/Event/TransitionEvent.php
@@ -1,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\StateTransitionMachine\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+use Elcodi\Component\StateTransitionMachine\Definition\Transition;
+use Elcodi\Component\StateTransitionMachine\Entity\Interfaces\StateableInterface;
+
+/**
+ * Class TransitionEvent
+ */
+class TransitionEvent extends Event
+{
+    /**
+     * @var StateableInterface
+     *
+     * Object
+     */
+    protected $object;
+
+    /**
+     * @var Transition
+     *
+     * Transition
+     */
+    protected $transition;
+
+    /**
+     * Construct
+     *
+     * @param StateableInterface $object     Object
+     * @param Transition         $transition Transition
+     */
+    public function __construct(StateableInterface $object, Transition $transition)
+    {
+        $this->object = $object;
+        $this->transition = $transition;
+    }
+
+    /**
+     * Get Object
+     *
+     * @return StateableInterface Object
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * get transition
+     *
+     * @return Transition Transition
+     */
+    public function getTransition()
+    {
+        return $this->transition;
+    }
+}

src/Elcodi/Component/StateTransitionMachine/Tests/UnitTest/Machine/MachineManagerTest.php
@@ -1,0 +1,114 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\StateTransitionMachine\Tests\UnitTest\Machine;
+
+use Elcodi\Component\StateTransitionMachine\Entity\StateLine;
+use Elcodi\Component\StateTransitionMachine\Tests\UnitTest\Fixtures\AbstractStateTransitionTest;
+use Elcodi\Component\StateTransitionMachine\Tests\UnitTest\Fixtures\Order;
+
+/**
+ * Class MachineManagerTest
+ */
+class MachineManagerTest extends AbstractStateTransitionTest
+{
+    /**
+     * Test initialize
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\ObjectAlreadyInitializedException
+     */
+    public function testInitializeNotEmpty()
+    {
+        $machineManager = $this->getMachineManager(0);
+
+        $order = new Order();
+        $stateLine = new StateLine();
+        $order->addStateLine($stateLine);
+        $machineManager->initialize($order, '');
+    }
+
+    /**
+     * Test initialize
+     */
+    public function testInitializeEmpty()
+    {
+        $machineManager = $this->getMachineManager(1);
+
+        $order = new Order();
+        $stateLine = $machineManager->initialize($order, '');
+        $this->assertEquals('unpaid', $stateLine->getName());
+    }
+
+    /**
+     * Test make transition with a non-initialized object
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\ObjectNotInitializedException
+     */
+    public function testMakeTransitionNonInitialized()
+    {
+        $machineManager = $this->getMachineManager(0);
+
+        $order = new Order();
+        $machineManager->makeTransition($order, 'pay', '');
+    }
+
+    /**
+     * Test make transition with an initialized object
+     */
+    public function testMakeTransitionInitialized()
+    {
+        $machineManager = $this->getMachineManager(4);
+
+        $order = new Order();
+        $machineManager->initialize($order, '');
+        $transition = $machineManager->makeTransition($order, 'pay', '');
+
+        $this->assertEquals('pay', $transition->getName());
+        $this->assertEquals('paid', $transition->getFinal()->getName());
+        $this->assertEquals('paid', $order->getLastStateLine()->getName());
+        $this->assertEquals('paid', $order->getStateLines()->last()->getName());
+    }
+
+    /**
+     * Test make transition with a non-initialized object
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\ObjectNotInitializedException
+     */
+    public function testReachStateNonInitialized()
+    {
+        $machineManager = $this->getMachineManager(0);
+
+        $order = new Order();
+        $machineManager->reachState($order, 'paid', '');
+    }
+
+    /**
+     * Test make transition with an initialized object
+     */
+    public function testReachStateInitialized()
+    {
+        $machineManager = $this->getMachineManager(4);
+
+        $order = new Order();
+        $machineManager->initialize($order, '');
+        $transition = $machineManager->reachState($order, 'paid', '');
+
+        $this->assertEquals('pay', $transition->getName());
+        $this->assertEquals('paid', $transition->getFinal()->getName());
+        $this->assertEquals('paid', $order->getLastStateLine()->getName());
+        $this->assertEquals('paid', $order->getStateLines()->last()->getName());
+    }
+}

src/Elcodi/Component/StateTransitionMachine/Tests/UnitTest/Machine/MachineTest.php
@@ -1,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\StateTransitionMachine\Tests\Machine;
+
+use Elcodi\Component\StateTransitionMachine\Tests\UnitTest\Fixtures\AbstractStateTransitionTest;
+
+/**
+ * Class MachineTest
+ */
+class MachineTest extends AbstractStateTransitionTest
+{
+    /**
+     * Test get machine id
+     */
+    public function testGetId()
+    {
+        $machine = $this->getMachine();
+
+        $this->assertEquals('id', $machine->getId());
+    }
+
+    /**
+     * Test get point of view
+     */
+    public function testGetPointOfEntry()
+    {
+        $machine = $this->getMachine();
+
+        $this->assertEquals('unpaid', $machine->getPointOfEntry());
+    }
+
+    /**
+     * Test make transition
+     */
+    public function testMakeTransitionOk()
+    {
+        $machine = $this->getMachine();
+
+        $transition = $machine->makeTransition('unpaid', 'pay');
+        $this->assertInstanceOf('Elcodi\Component\StateTransitionMachine\Definition\Transition', $transition);
+        $this->assertEquals('unpaid', $transition->getStart()->getName());
+        $this->assertEquals('pay', $transition->getName());
+        $this->assertEquals('paid', $transition->getFinal()->getName());
+    }
+
+    /**
+     * Test make transition with exception
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\TransitionNotAccessibleException
+     */
+    public function testMakeTransitionException()
+    {
+        $machine = $this->getMachine();
+
+        $machine->makeTransition('unpaid', 'ship');
+    }
+
+    /**
+     * Test reach state
+     */
+    public function testReachStateOk()
+    {
+        $machine = $this->getMachine();
+
+        $transition = $machine->reachState('unpaid', 'paid');
+        $this->assertInstanceOf('Elcodi\Component\StateTransitionMachine\Definition\Transition', $transition);
+        $this->assertEquals('unpaid', $transition->getStart()->getName());
+        $this->assertEquals('pay', $transition->getName());
+        $this->assertEquals('paid', $transition->getFinal()->getName());
+    }
+
+    /**
+     * Test reach state with exception
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\StateNotReachableException
+     */
+    public function testReachStateException()
+    {
+        $machine = $this->getMachine();
+
+        $machine->reachState('unpaid', 'shipped');
+    }
+}

elcodi/StateTransitionMachine

Event/InitializationEvent.php
@@ -1,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\StateTransitionMachine\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+use Elcodi\Component\StateTransitionMachine\Entity\Interfaces\StatefulInterface;
+
+/**
+ * Class InitializationEvent
+ */
+class InitializationEvent extends Event
+{
+    /**
+     * @var StatefulInterface
+     *
+     * Object
+     */
+    protected $object;
+
+    /**
+     * Construct
+     *
+     * @param StatefulInterface $object Object
+     */
+    public function __construct(StatefulInterface $object)
+    {
+        $this->object = $object;
+    }
+
+    /**
+     * Get Object
+     *
+     * @return StatefulInterface Object
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+}

Event/TransitionEvent.php
@@ -1,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\StateTransitionMachine\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+use Elcodi\Component\StateTransitionMachine\Definition\Transition;
+use Elcodi\Component\StateTransitionMachine\Entity\Interfaces\StatefulInterface;
+
+/**
+ * Class TransitionEvent
+ */
+class TransitionEvent extends Event
+{
+    /**
+     * @var StatefulInterface
+     *
+     * Object
+     */
+    protected $object;
+
+    /**
+     * @var Transition
+     *
+     * Transition
+     */
+    protected $transition;
+
+    /**
+     * Construct
+     *
+     * @param StatefulInterface $object     Object
+     * @param Transition        $transition Transition
+     */
+    public function __construct(StatefulInterface $object, Transition $transition)
+    {
+        $this->object = $object;
+        $this->transition = $transition;
+    }
+
+    /**
+     * Get Object
+     *
+     * @return StatefulInterface Object
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * get transition
+     *
+     * @return Transition Transition
+     */
+    public function getTransition()
+    {
+        return $this->transition;
+    }
+}

Tests/UnitTest/Machine/MachineManagerTest.php
@@ -1,0 +1,114 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\StateTransitionMachine\Tests\UnitTest\Machine;
+
+use Elcodi\Component\StateTransitionMachine\Entity\StateLine;
+use Elcodi\Component\StateTransitionMachine\Tests\UnitTest\Fixtures\AbstractStateTransitionTest;
+use Elcodi\Component\StateTransitionMachine\Tests\UnitTest\Fixtures\Order;
+
+/**
+ * Class MachineManagerTest
+ */
+class MachineManagerTest extends AbstractStateTransitionTest
+{
+    /**
+     * Test initialize
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\ObjectAlreadyInitializedException
+     */
+    public function testInitializeNotEmpty()
+    {
+        $machineManager = $this->getMachineManager(0);
+
+        $order = new Order();
+        $stateLine = new StateLine();
+        $order->addStateLine($stateLine);
+        $machineManager->initialize($order, '');
+    }
+
+    /**
+     * Test initialize
+     */
+    public function testInitializeEmpty()
+    {
+        $machineManager = $this->getMachineManager(1);
+
+        $order = new Order();
+        $stateLine = $machineManager->initialize($order, '');
+        $this->assertEquals('unpaid', $stateLine->getName());
+    }
+
+    /**
+     * Test make transition with a non-initialized object
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\ObjectNotInitializedException
+     */
+    public function testMakeTransitionNonInitialized()
+    {
+        $machineManager = $this->getMachineManager(0);
+
+        $order = new Order();
+        $machineManager->transition($order, 'pay', '');
+    }
+
+    /**
+     * Test make transition with an initialized object
+     */
+    public function testMakeTransitionInitialized()
+    {
+        $machineManager = $this->getMachineManager(4);
+
+        $order = new Order();
+        $machineManager->initialize($order, '');
+        $transition = $machineManager->transition($order, 'pay', '');
+
+        $this->assertEquals('pay', $transition->getName());
+        $this->assertEquals('paid', $transition->getFinal()->getName());
+        $this->assertEquals('paid', $order->getLastStateLine()->getName());
+        $this->assertEquals('paid', $order->getStateLines()->last()->getName());
+    }
+
+    /**
+     * Test make transition with a non-initialized object
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\ObjectNotInitializedException
+     */
+    public function testReachStateNonInitialized()
+    {
+        $machineManager = $this->getMachineManager(0);
+
+        $order = new Order();
+        $machineManager->reachState($order, 'paid', '');
+    }
+
+    /**
+     * Test make transition with an initialized object
+     */
+    public function testReachStateInitialized()
+    {
+        $machineManager = $this->getMachineManager(4);
+
+        $order = new Order();
+        $machineManager->initialize($order, '');
+        $transition = $machineManager->reachState($order, 'paid', '');
+
+        $this->assertEquals('pay', $transition->getName());
+        $this->assertEquals('paid', $transition->getFinal()->getName());
+        $this->assertEquals('paid', $order->getLastStateLine()->getName());
+        $this->assertEquals('paid', $order->getStateLines()->last()->getName());
+    }
+}

Tests/UnitTest/Machine/MachineTest.php
@@ -1,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\StateTransitionMachine\Tests\Machine;
+
+use Elcodi\Component\StateTransitionMachine\Tests\UnitTest\Fixtures\AbstractStateTransitionTest;
+
+/**
+ * Class MachineTest
+ */
+class MachineTest extends AbstractStateTransitionTest
+{
+    /**
+     * Test get machine id
+     */
+    public function testGetId()
+    {
+        $machine = $this->getMachine();
+
+        $this->assertEquals('id', $machine->getId());
+    }
+
+    /**
+     * Test get point of view
+     */
+    public function testGetPointOfEntry()
+    {
+        $machine = $this->getMachine();
+
+        $this->assertEquals('unpaid', $machine->getPointOfEntry());
+    }
+
+    /**
+     * Test make transition
+     */
+    public function testMakeTransitionOk()
+    {
+        $machine = $this->getMachine();
+
+        $transition = $machine->transition('unpaid', 'pay');
+        $this->assertInstanceOf('Elcodi\Component\StateTransitionMachine\Definition\Transition', $transition);
+        $this->assertEquals('unpaid', $transition->getStart()->getName());
+        $this->assertEquals('pay', $transition->getName());
+        $this->assertEquals('paid', $transition->getFinal()->getName());
+    }
+
+    /**
+     * Test make transition with exception
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\TransitionNotAccessibleException
+     */
+    public function testMakeTransitionException()
+    {
+        $machine = $this->getMachine();
+
+        $machine->transition('unpaid', 'ship');
+    }
+
+    /**
+     * Test reach state
+     */
+    public function testReachStateOk()
+    {
+        $machine = $this->getMachine();
+
+        $transition = $machine->reachState('unpaid', 'paid');
+        $this->assertInstanceOf('Elcodi\Component\StateTransitionMachine\Definition\Transition', $transition);
+        $this->assertEquals('unpaid', $transition->getStart()->getName());
+        $this->assertEquals('pay', $transition->getName());
+        $this->assertEquals('paid', $transition->getFinal()->getName());
+    }
+
+    /**
+     * Test reach state with exception
+     *
+     * @expectedException \Elcodi\Component\StateTransitionMachine\Exception\StateNotReachableException
+     */
+    public function testReachStateException()
+    {
+        $machine = $this->getMachine();
+
+        $machine->reachState('unpaid', 'shipped');
+    }
+}